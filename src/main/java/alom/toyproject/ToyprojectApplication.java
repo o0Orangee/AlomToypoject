9e2f836b7f05aff1040f032f23e4d0333a515c4f
o0Orangee <maboongee@gmail.com>
2024-03-05 21:26:59 +0900

    first commit

---
+++ src/main/java/alom/toyproject/ToyprojectApplication.java
@@ -0,0 +1,13 @@
+package alom.toyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyprojectApplication.class, args);
+	}
+
+}
